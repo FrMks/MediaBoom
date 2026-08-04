baf6ee9d41f72b8b4269703d134edd81f3861ff9
FrMks <maxfrancuzov@gmail.com>
2026-08-04 11:45:51 +0300

    feat(#1): add static create method in User entity

--- src/backend/MediaBoomService.Domain/Users/User.cs
+++ src/backend/MediaBoomService.Domain/Users/User.cs
@@ -2,8 +2,12 @@ using MediaBoomService.Domain.Users.ValueObjects;
 
 namespace MediaBoomService.Domain.Users;
 
+/// <summary>
+/// Represents a user in the system.
+/// </summary>
 public sealed class User
 {
+    // Ef core
     private User() { }
 
     private User(
@@ -22,15 +26,54 @@ public sealed class User
         Username = username;
     }
 
+    /// <summary>
+    /// Gets the unique identifier of the user.
+    /// </summary>
     public Guid Id { get; private set; }
 
+    /// <summary>
+    /// Gets the email address of the user.
+    /// </summary>
     public Email Email { get; private set; }
 
+    /// <summary>
+    /// Gets the URL of the user's logo.
+    /// </summary>
     public string LogoUrl { get; private set; }
 
+    /// <summary>
+    /// Gets the UTC timestamp when the user was created.
+    /// </summary>
     public DateTime CreatedAtUtc { get; private set; }
 
+    /// <summary>
+    /// Gets the hashed password of the user.
+    /// </summary>
     public string PasswordHash { get; private set; }
 
+    /// <summary>
+    /// Gets the username of the user.
+    /// </summary>
     public string Username { get; private set; }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="User"/> class with the specified properties.
+    /// </summary>
+    /// <param name="id">The unique identifier of the user.</param>
+    /// <param name="email">The email address of the user.</param>
+    /// <param name="logoUrl">The URL of the user's logo.</param>
+    /// <param name="createdAtUtc">The UTC timestamp when the user was created.</param>
+    /// <param name="passwordHash">The hashed password of the user.</param>
+    /// <param name="username">The username of the user.</param>
+    /// <returns></returns>
+    public static User Create(
+        Guid id,
+        Email email,
+        string logoUrl,
+        DateTime createdAtUtc,
+        string passwordHash,
+        string username)
+    {
+        return new User(id, email, logoUrl, createdAtUtc, passwordHash, username);
+    }
 }
